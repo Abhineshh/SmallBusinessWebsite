9eedb02c98ff7771aebb68224b193d5275dc7a90
Abhinesh <abhineshwill@gmail.com>
2023-12-01 11:06:54 +0530

    minor update

--- businessServer/index.js
+++ businessServer/index.js
@@ -1,22 +1,43 @@
 const express = require('express');
-
-
-const cors = require('cors');
-
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const app = express();
-
-
+app.use(bodyParser.json());
 app.use(cors());
 
-app.post("/mailadmin",(req,res)=>{
-    const a = req.body;
-    console.log(a);
-   res.json({status:'ding dong'})
-});    
 
+app.post('/sendEmail', (req, res) => {
+  const { to, subject, message } = req.body;
 
-app.listen(3000,() =>{
-    
-    console.log('server is running on port 3000');
+  // Configure nodemailer (replace with your email service details)
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'your-email@gmail.com',
+      pass: 'your-email-password'
+    }
+  });
+
+  const mailOptions = {
+    from: 'your-email@gmail.com',
+    to: to,
+    subject: subject,
+    text: message
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+      res.status(500).send('Error sending email');
+    } else {
+      console.log('Email sent:', info.response);
+      res.status(200).send('Email sent');
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
 });
